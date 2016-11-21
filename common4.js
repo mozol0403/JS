d34002a2dfea324f93796bc9c3a06a2b4d24f573
Yulia Mozol <yulya.mezol@gmail.com>
2016-11-21 17:53:26 +0200

    commit4

---
+++ common4.js
@@ -0,0 +1,26 @@
+var number = 234.667;
+n=NaN;
+inf = Infinity;
+
+//number.toFixed(2); //скільки чисел виводити після коми
+//console.log(number.toFixed(2) );
+//console.log(number.toExponential() ); //геометрична прогресія
+//console.log(number.toPrecision(5) ); //округлює до загальної к-ті цифр -- 234
+//console.log( typeof number.toString(2) ); //Привести число к строке
+//console.log(Math.sqrt(9));
+//console.log(Math.pow(5, 3)); // 125 - 5 в 3 ступені(5 в кубі)
+//методи округлення:
+//console.log(Math.floor(number)); // - 234
+//console.log(Math.ceil(number));   // - 235
+//console.log(Math.round(number));  // - 234 - ПРАВИЛЬНИЙ
+//console.log(Math.PI);
+
+//console.log('cf90'/5+1); // =NaN - число не = нічому
+//console.log(NaN===NaN);
+//console.log(isNaN(n)); //-true щоб перевірити чи функція є NaN
+//console.log(isNaN(number)); //false
+
+console.log(1/0);
+console.log(inf>10246666); - true
+console.log(typeof inf); // number
+console.log(inf+4539); // = Infinity
