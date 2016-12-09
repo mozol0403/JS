4476cfc3198ac2a91810d8006d97b6aadb3ba6a2
Yulia Mozol <yulya.mezol@gmail.com>
2016-12-09 16:12:47 +0200

    common12(9/12)

--- common12.js
+++ common12.js
@@ -1,12 +1,14 @@
+$(document).ready(function(){
 // Дозволяє багато раз використовувати один і той же шматок коду, для скрития кода і построєння програм
 // ф-ція складається з 4-х частин:
-/* 1. function */
+    /* 1. function */
 // 2. func
 // 3. ()
 // 4. { інструкція; }
-function funcDec ( a, b) {   //   a,b - параметри
- return a + b; // без return == буде undefined     ===== фанкшен декларейшн -- має ХОСТІНГ
-}
+    function funcDec(a, b) {   //   a,b - параметри
+        return a + b; // без return == буде undefined     ===== фанкшен декларейшн -- має ХОСТІНГ
+    }
+
 //console.log( funcDec(3,4) );  // 3,4 - аргументи
 
 //console.log( funcDec ); // == код цієї ф-ці
@@ -16,47 +18,47 @@ function funcDec ( a, b) {   //   a,b - параметри
 
 // ===== фанкшен експрешен - літерал функції (визначення ф-ції): спочатку створюємо змінну, а потім присвоюємо їй ф-цію, в основн анонімну
 
-var funcExp = function ( a, b ) {
-    return a + b;
-};
+    var funcExp = function (a, b) {
+        return a + b;
+    };
 //console.log( funcExp(2,3) );
 
 // ХОСТІНГ :
- function func1() {
-     function funcExample() {
-        return 'one';
-     }
+    function func1() {
+        function funcExample() {
+            return 'one';
+        }
 
-     return funcExample();
+        return funcExample();
 
-     function funcExample() {
-         return 'two';
-     }
- };
+        function funcExample() {
+            return 'two';
+        }
+    };
 
- //console.log( func1() ); // === two - виклик функції раніше її визначення
+    //console.log( func1() ); // === two - виклик функції раніше її визначення
 
 // хостінг з фанкшен експрешен
-function func2() {
-     var funcExample = function() {
-        return 'one';
-    }
+    function func2() {
+        var funcExample = function () {
+            return 'one';
+        }
 
-    return funcExample();
+        return funcExample();
 
-   var funcExample = function () {
-        return 'two';
-    }
-};
+        var funcExample = function () {
+            return 'two';
+        }
+    };
 // console.log( func1() ); // === two
 // console.log( func2() ); // === one
 
 
 // Ф-ЦІЯ ЗВОРОТНЬОГО ВИКЛИКУ - виконати ф після якоїсь події:
-var func = function ( callback) {
- var name = "Nick";
-    return callback(name);
- };
+    var func = function (callback) {
+        var name = "Nick";
+        return callback(name);
+    };
 
 
 // console.log( func( function(n){
@@ -64,30 +66,36 @@ var func = function ( callback) {
 // }) );
 
 // ПОВЕРНЕННЯ ЦІЛОЇ Ф-ЦІЇ
-var func1 = function () {
-    return function () {
-        console.log( 'Hiiii');
+    var func1 = function () {
+        return function () {
+            console.log('Hiiii');
+        };
     };
-    };
- //func1 ()(); // Hiiii
+    //func1 ()(); // Hiiii
 
 // АНОНІМНА САМОВИКЛИКАЮЧА Ф-ЦІЯ
-var prop = 1;
+    var prop = 1;
 //console.log(prop);
-( function () {
- //console.log('Ky-ky')
-})();
+    (function () {
+        //console.log('Ky-ky')
+    })();
 
 
 // ОБ'ЄКТ, КОСЯЩИЙ ПІД МАСИВ
-var funcArgs = function ( ) {
-    console.log(arguments);
-    var i,
-        sum = 0;
-    for  ( i = 0; i < arguments.length; i++) {
-        sum += arguments[i];
-          };
-  return sum;
-};
-console.log( funcArgs( 1,  2, 3, 15) ) ;
+    var funcArgs = function () {
+        console.log(arguments);
+        var i,
+            sum = 0;
+        for (i = 0; i < arguments.length; i++) {
+            sum += arguments[i];
+        }
+        ;
+        return sum;
+    };
+    console.log(funcArgs(1, 2, 3, 15));
+
+    $(document).ready
+    $("h1").text("YULIA Mozol");
+});
+
 
